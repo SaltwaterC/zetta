818803246fd54fe3da5f60f96e7ea12463e19db6
SaltwaterC <saltwaterc@gmail.com>
2026-08-11 01:26:50 +0100

    Fix Linux build

--- src/silent_mode.rs
+++ src/silent_mode.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 #[cfg(target_os = "linux")]
 use std::process::Command;
 
-use gpui::{App, Context, Entity, Window};
+use gpui::{App, AppContext, Context, Entity, Window};
 use terminal_view::TerminalView;
 
 use crate::{
